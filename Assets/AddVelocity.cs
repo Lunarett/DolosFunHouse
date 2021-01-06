3314a3e958e8989555a182c28e55d0465e02dffa
Egor <e.ageev@saedubai.com>
2021-01-06 19:31:48 +0400

    Level: Zone_A complete

---
+++ Assets/AddVelocity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AddVelocity : MonoBehaviour
+{
+	[SerializeField] Vector3 velocity;
+	[SerializeField] bool update;
+
+	Rigidbody rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void Start()
+	{
+		rb.AddForce(velocity, ForceMode.Force);
+	}
+
+	private void Update()
+	{
+		if(update)
+			rb.AddForce(velocity, ForceMode.Force);
+	}
+}
