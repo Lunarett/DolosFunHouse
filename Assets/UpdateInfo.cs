9d9e8de8e7f6e6577a675b80e504461741c22b0d
Egor <e.ageev@saedubai.com>
2021-01-11 18:24:41 +0400

    Added: Title Empty space check & Spawn button on create

---
+++ Assets/UpdateInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Michsky.UI.Dark
+{
+	public class UpdateInfo : MonoBehaviour
+	{
+		[SerializeField] private CreateLobby _createLobby;
+		[Space]
+		[SerializeField] private TextMeshProUGUI _title;
+		[SerializeField] private TextMeshProUGUI _desc;
+
+		private void Start()
+		{
+			_title.text = _createLobby.TitleText.text;
+			_desc.text = _createLobby.DescText.text;
+		}
+	}
+}
