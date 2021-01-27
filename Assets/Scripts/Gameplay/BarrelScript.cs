44db885dbc920037ecd712ab1dd876d7b9c0238f
Berzerk <bleser.florent@gmail.com>
2021-01-27 10:50:59 +0100

    -put my scripts into a gameplay folder -made basic game manager that spawns player instances

---
+++ Assets/Scripts/Gameplay/BarrelScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+
+public class BarrelScript : Interactible
+{
+    private GameObject _player = null;
+    private bool _isOccupied = false;
+    private InputMap _inputMap;
+
+    [SerializeField] private Transform _exitTransform;
+    [SerializeField] private Camera _peekCam;
+
+    [SerializeField] private float _mouseSensitivity = 50f;
+    [SerializeField] private float _yRotClamp = 90f;
+
+
+    private float _yRotation = 0f;
+
+    public void Awake()
+    {
+        InitInput();
+    }
+
+    public void Start()
+    {
+        _inputMap.Disable();
+    }
+
+    public override void Interact(GameObject playerObject)
+    {
+        base.Interact(playerObject);
+
+        if (!_isOccupied)
+        {
+            EnterBarrel(playerObject);
+        }
+    }
+
+    private void MovePlayer(Transform destination)
+    {
+        _player.transform.position = destination.position;
+        _player.transform.rotation = destination.rotation;
+    }
+    private void EnterBarrel(GameObject playerObject)
+    {
+        OnEnable();
+        //move player character
+        _player = playerObject;
+        MovePlayer(transform);
+        PlayerController playerController = _player.GetComponent<PlayerController>();
+
+        _isOccupied = true;
+
+        //disable player control
+        playerController.ToggleCharacterActive();
+
+        playerController.SwitchActiveCam(_peekCam);
+    }
+    void ExitBarrel()
+    {
+        if (_exitTransform != null)
+        {
+            MovePlayer(_exitTransform);
+
+            _isOccupied = false;
+            PlayerController playerController = _player.GetComponent<PlayerController>();
+
+            playerController.ToggleCharacterActive();
+
+            playerController.SwitchActiveCam(_peekCam);
+
+            _player = null;
+        }
+        else
+        {
+            Debug.Log(gameObject.name + " missing exit transform!");
+        }
+        OnDisable();
+    }
+
+    private void CamFollowMouse(Vector2 mouseInput)
+    {
+        //we receive a Vector 2 where x is the horizontal and y the vertical movement of the mouse over time (delta)
+        float moveX = mouseInput.x * _mouseSensitivity * Time.deltaTime;
+
+
+        _yRotation = Mathf.Clamp(_yRotation + moveX, -_yRotClamp, _yRotClamp);
+
+        _peekCam.gameObject.transform.Rotate(Vector3.up * _yRotation * Time.deltaTime);
+        _peekCam.gameObject.transform.localRotation = Quaternion.Euler(0f, _yRotation, 0f);
+    }
+
+    private void InitInput()
+    {
+        _inputMap = new InputMap();
+        _inputMap.Barrel.MouseDelta.performed += context => CamFollowMouse(context.ReadValue<Vector2>());
+        _inputMap.Barrel.Leave.performed += context => ExitBarrel();
+    }
+
+    private void OnEnable()
+    {
+        _inputMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputMap.Disable();
+    }
+}
---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private PlayerController[] _players;
+    [SerializeField] private GameObject _playerPrefab;
+
+    [SerializeField] private Transform _spawnPoint;
+
+
+    void Start()
+    {
+        _players = new PlayerController[4];
+
+        SpawnPlayers(4);
+    }
+
+    public void SpawnPlayers(int ammount)
+    {
+        Vector3 spacing = new Vector3(2, 0, 0);
+        
+        for(int i = 0; i < ammount; i++)
+        {
+            _players[i] = Instantiate(_playerPrefab, _spawnPoint.position + spacing * i, Quaternion.identity).GetComponentInChildren<PlayerController>();
+            //_players[i].transform.parent.gameObject.transform.position += spacing * i;
+            Debug.Log(_players[i].transform.parent.gameObject.name);
+        }
+
+        Debug.Log("Spawned " + ammount + " players.");
+    }
+}
