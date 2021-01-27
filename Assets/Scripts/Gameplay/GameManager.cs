a4cef7bdfe51323f6c900526e4449cea3c79d1ab
Berzerk <bleser.florent@gmail.com>
2021-01-27 13:44:59 +0100

    quick push

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,33 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
     private PlayerController[] _players;
     [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private string _playerPrefabName;
 
     [SerializeField] private Transform _spawnPoint;
 
 
     void Start()
     {
-        _players = new PlayerController[4];
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.OfflineMode = true;
+        }
+        else
+        {
+            SpawnPlayer();
+        }
 
-        SpawnPlayers(4);
     }
 
+    /*
     public void SpawnPlayers(int ammount)
     {
+        _players = new PlayerController[ammount];
+
         Vector3 spacing = new Vector3(2, 0, 0);
-        
-        for(int i = 0; i < ammount; i++)
+
+        if (PhotonNetwork.OfflineMode)
         {
-            _players[i] = Instantiate(_playerPrefab, _spawnPoint.position + spacing * i, Quaternion.identity).GetComponentInChildren<PlayerController>();
-            //_players[i].transform.parent.gameObject.transform.position += spacing * i;
-            Debug.Log(_players[i].transform.parent.gameObject.name);
+            for (int i = 0; i < ammount; i++)
+            {
+                _players[i] = Instantiate(_playerPrefab, _spawnPoint.position + spacing * i, Quaternion.identity).GetComponentInChildren<PlayerController>();
+                //_players[i].transform.parent.gameObject.transform.position += spacing * i;
+                Debug.Log(_players[i].transform.parent.gameObject.name);
+            }
         }
 
         Debug.Log("Spawned " + ammount + " players.");
+    } */
+    public void SpawnPlayer()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Instantiate(_playerPrefabName, _spawnPoint.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_playerPrefab, _spawnPoint.position, Quaternion.identity);
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        if (PhotonNetwork.OfflineMode)
+        {
+            PhotonNetwork.CreateRoom("OfflineMode");
+        }
+    }
+
+    public override void OnCreatedRoom()
+    {
+        if (PhotonNetwork.OfflineMode)
+        {
+            SpawnPlayer();
+        }
     }
 }
